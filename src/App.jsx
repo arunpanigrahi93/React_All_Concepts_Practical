9f0e02d8801dc36dc57347e9a25f22185c4221ec
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-24 19:38:03 +0530

    React hooks useEffect with setTimeout cb

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import UseStateHook from "./components/hooks/UseStateHook";
+import UseEffectHook from "./components/hooks/UseEffectHook";
 
 function App() {
   return (
     <div>
       <UseStateHook />
+      <UseEffectHook />
     </div>
   );
 }
---
+++ src/components/hooks/UseEffectHook.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function UseEffectHook() {
+  const [value, setValue] = useState("Hi");
+
+  useEffect(() => {
+    const newValue = "This is Arun";
+
+    setTimeout(() => {
+      setValue(newValue);
+    }, 3000);
+  }, []);
+
+  return (
+    <div>
+      <h2>{value}</h2>
+    </div>
+  );
+}
+export default UseEffectHook;
