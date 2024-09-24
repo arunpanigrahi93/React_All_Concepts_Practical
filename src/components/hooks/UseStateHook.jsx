823a2a79d9beedfa50ee93c1309a60bb6f659736
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-24 19:18:04 +0530

    React hooks useState

---
+++ src/components/hooks/UseStateHook.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function UseStateHook() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      {count}
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+export default UseStateHook;
