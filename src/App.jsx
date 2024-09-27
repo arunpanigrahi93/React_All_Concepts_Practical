cd01ff2bcf785ddc00cfeb445387501a2669c6f8
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-27 16:19:44 +0530

    portals setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import UseStateHook from "./components/hooks/UseStateHook";
 import UseEffectHook from "./components/hooks/UseEffectHook";
 import Parent from "./components/hooks/MemoCallbackUseMemo/Parent";
+import Portal from "./components/hooks/Portal.jsx";
 
 function App() {
   return (
     <div>
       {/* <UseStateHook />
       <UseEffectHook /> */}
-      <Parent />
+      {/* <Parent /> */}
+      <Portal />
     </div>
   );
 }
