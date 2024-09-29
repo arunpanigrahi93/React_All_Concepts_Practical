af0bff8ad05b07ca7097f77f7652e455c8a48638
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-29 20:51:27 +0530

    accordian button functionality

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,8 +3,14 @@ import data from "./data";
 
 function Accordion() {
   const [show, setShow] = useState(false);
+  const [desc, setDesc] = useState(false);
 
-  const showHandler = () => {};
+  const showHandler = () => {
+    setShow(!show);
+    setDesc(!desc);
+
+    console.log("Accordion clicked");
+  };
 
   return (
     <div>
@@ -14,6 +20,7 @@ function Accordion() {
             <li key={item.id}>
               <h2>{item.question}</h2>
               <button onClick={showHandler}>+</button>
+              {show && <p>{desc ? item.answer : ""}</p>}
             </li>
           </div>
         ))}
