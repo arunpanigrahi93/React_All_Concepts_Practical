f2998ee8672df953bdb2fb6d6c222ceff8a2b6e6
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-10-03 17:30:33 +0530

    ramdomcolor_generator_layout

--- src/components/Random-color/index.jsx
+++ src/components/Random-color/index.jsx
@@ -1,4 +1,18 @@
+// #456789
+// rgb(25,45,67)
+
+import { useState } from "react";
+
 function RandomColor() {
-  return <div>Random clr</div>;
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  return (
+    <div style={{ width: "100vw", height: "100vh", background: color }}>
+      <button>Create HEX Color</button>
+      <button>Create RGB Color</button>
+      <button>Generate Random Color</button>
+    </div>
+  );
 }
 export default RandomColor;
