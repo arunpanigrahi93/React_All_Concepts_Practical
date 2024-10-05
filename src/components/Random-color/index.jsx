7b9b8eb7955a4b884582cc32ec0d8cf16fc394a8
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-10-05 20:42:37 +0530

    ramdomcolor_generator_layout

--- src/components/Random-color/index.jsx
+++ src/components/Random-color/index.jsx
@@ -27,7 +27,7 @@ function RandomColor() {
   return (
     <div style={{ width: "100vw", height: "100vh", background: color }}>
       <button onClick={() => setTypeOfColor("hex")}>Create HEX Color</button>
-      <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button>
+      {/* <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button> */}
       <button
         onClick={
           typeOfColor === "hex"
